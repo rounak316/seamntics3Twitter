5528ec822c42e3f693ba44513ae1362b999487c4
Prakhar Agnihotri <prakhar.agnihotri@timesinternet.in>
2019-08-07 03:50:58 +0530

    ALL API IMPLEMENTED

--- dist/routES/tweets.js
+++ dist/routES/tweets.js
@@ -7,6 +7,115 @@ const express_1 = __importDefault(require("express"));
 const db_1 = require("../mongodb/db");
 const util_1 = require("util");
 const router = express_1.default.Router();
+// Get Approchability  
+router.get("/approchableUsers", (req, res) => {
+    db_1.Tweet.aggregate([
+        {
+            "$match": {
+                "entities.user_mentions.1": { "$exists": true },
+            }
+        },
+        {
+            "$unwind": "$entities.user_mentions",
+        },
+        {
+            "$project": {
+                screen_name: "$user.screen_name",
+                verified: { "$toInt": { "$ifNull": ["$entities.user_mentions.verified", 0] } },
+                mentioned: { "$toInt": "1" },
+            }
+        },
+        {
+            "$group": {
+                "_id": "$screen_name",
+                "total": { "$sum": 1 },
+                // "mentioned":  {"$sum":{ "$size" :  "$entities.user_mentions" } },
+                "totalCount": { "$sum": "$mentioned" },
+                "verifiedCount": { "$sum": "$verified" },
+            }
+        },
+        {
+            "$project": {
+                _id: 0,
+                screen_name: "$_id",
+                total: "$total",
+                totalCount: "$totalCount",
+                verifiedCount: "$verifiedCount",
+                approchability: { "$multiply": [{ "$divide": [{ "$subtract": ["$total", "$verifiedCount"] }, "$total"] }, 100] }
+            }
+        },
+        {
+            "$sort": {
+                "approchability": -1
+            }
+        }
+    ]).then(data => {
+        res.send({ status: true, data: data });
+    }).catch(err => {
+        res.send({ status: false, error: err.message });
+    });
+});
+// Get Leaderboard  
+router.post("/leaderboards", (req, res) => {
+    let from = req.body["from"];
+    let to = req.body["to"];
+    let fromWeek = (((from["week"] * 7) - 7) + 1) % 32;
+    let toWeek = (to["week"] * 7) % 32;
+    let fromHour = from["hour"] % 25;
+    let toHour = to["hour"] % 25;
+    let fromMonth = from["month"] % 13;
+    let toMonth = to["month"] % 13;
+    let matchQuery = {};
+    if (util_1.isNumber(fromMonth) && util_1.isNumber(toMonth)) {
+        matchQuery["month"] = { "$gte": fromMonth, "$lte": toMonth };
+    }
+    if (util_1.isNumber(fromWeek) && util_1.isNumber(toWeek)) {
+        matchQuery["day"] = { "$gte": fromWeek, "$lte": toWeek };
+    }
+    if (util_1.isNumber(fromHour) && util_1.isNumber(toHour)) {
+        matchQuery["hour"] = { "$gte": fromHour, "$lte": toHour };
+    }
+    {
+        db_1.Tweet.aggregate([
+            {
+                "$project": {
+                    year: { $year: "$created_at" },
+                    month: { $month: "$created_at" },
+                    day: { $dayOfMonth: "$created_at" },
+                    hour: { $hour: "$created_at" },
+                    minutes: { $minute: "$created_at" },
+                    seconds: { $second: "$created_at" },
+                    milliseconds: { $millisecond: "$created_at" },
+                    dayOfYear: { $dayOfYear: "$created_at" },
+                    dayOfWeek: { $dayOfWeek: "$created_at" },
+                    text: "$text",
+                    source: "$source",
+                    screenName: "$user.screen_name",
+                    retweet_count: { "$ifNull": ["$retweet_count", 0] }
+                }
+            },
+            {
+                "$sort": {
+                    "retweet_count": -1
+                }
+            },
+            {
+                "$group": { "_id": "$screenName",
+                    "retweets": { $first: "$retweet_count" }
+                }
+            },
+            {
+                "$sort": {
+                    "retweets": -1
+                }
+            }
+        ]).then(data => {
+            res.send({ status: true, data: data });
+        }).catch(err => {
+            res.send({ status: false, error: err.message });
+        });
+    }
+});
 // Get   tweets between 
 router.post("/get/:username", (req, res) => {
     let screenName = req.params["username"];
--- src/routes/tweets.ts
+++ src/routes/tweets.ts
@@ -5,6 +5,180 @@ import { isNumber } from "util";
 const router = express.Router();
 
 
+// Get Approchability  
+router.get("/approchableUsers" , (req: any, res: any) => {
+
+
+        
+        Tweet.aggregate( 
+            [
+
+
+                 {
+                    "$match":
+                    {
+                        "entities.user_mentions.1": {  "$exists": true }  ,
+
+                    }
+                }
+                ,
+                {
+                    "$unwind": "$entities.user_mentions",
+                }
+,
+                 
+                {
+                   
+                    "$project": {
+                        screen_name: "$user.screen_name",
+                        verified: {  "$toInt":  {"$ifNull" : ["$entities.user_mentions.verified", 0]} } ,
+                        mentioned: {  "$toInt": "1" },
+
+                    } 
+                },
+
+                {
+                        "$group": {
+                            "_id": "$screen_name" ,
+                            "total": {"$sum": 1 },
+                            // "mentioned":  {"$sum":{ "$size" :  "$entities.user_mentions" } },
+                            "totalCount":  {"$sum": "$mentioned" },
+                            "verifiedCount":  {"$sum": "$verified" },
+    
+    
+                        // userMentioned: { $push:  { item: "$entities.user_mentions" } }
+                    }
+                }
+                ,
+                {
+                    "$project": {
+                        _id: 0,
+                        screen_name: "$_id",
+                        total: "$total",
+                        totalCount: "$totalCount",
+                        verifiedCount: "$verifiedCount",
+                        approchability: { "$multiply":  [  {"$divide": [  { "$subtract" :["$total"  ,  "$verifiedCount" ] }  , "$total"  ]  }  , 100]}
+                    }
+                }
+                ,
+                {
+                    "$sort": {
+                        "approchability": -1
+                    }
+                }
+
+   
+
+
+        ]
+              ).then( data=>{
+
+                res.send( {status: true , data: data});
+            
+            
+           
+        } ).catch( err=>{
+            res.send( {status: false  , error: err.message});
+        } )
+    
+});
+
+
+// Get Leaderboard  
+router.post("/leaderboards" , (req: any, res: any) => {
+
+
+    let from = req.body["from"]
+    let to = req.body["to"]
+
+    let fromWeek = (( ( from["week"] * 7 )- 7 )  + 1 ) % 32
+    let toWeek = ( to["week"] * 7 )% 32
+
+
+    let fromHour = from["hour"] % 25
+    let toHour = to["hour"] % 25
+
+
+
+    let fromMonth = from["month"] % 13
+    let toMonth = to["month"] % 13
+
+
+    let matchQuery : any = {}
+
+    if( isNumber(fromMonth) && isNumber(toMonth)  ){
+        matchQuery[ "month" ] = { "$gte": fromMonth , "$lte": toMonth }
+    }
+
+    if( isNumber(fromWeek) && isNumber(toWeek)  ){
+        matchQuery[ "day" ] = { "$gte": fromWeek , "$lte": toWeek }
+    }
+
+    if( isNumber(fromHour) && isNumber(toHour)  ){
+        matchQuery[ "hour" ] = { "$gte": fromHour , "$lte": toHour }
+    }
+
+
+
+
+
+        {
+        Tweet.aggregate( 
+            [
+                {
+                    "$project"  :
+                    {
+                    year: { $year: "$created_at" },
+                    month: { $month: "$created_at" },
+                    day: { $dayOfMonth: "$created_at" },
+                    hour: { $hour: "$created_at" },
+                    minutes: { $minute: "$created_at" },
+                    seconds: { $second: "$created_at" },
+                    milliseconds: { $millisecond: "$created_at" },
+                    dayOfYear: { $dayOfYear: "$created_at" },
+                    dayOfWeek: { $dayOfWeek: "$created_at" },
+                    text: "$text",
+                    source: "$source",
+                    screenName: "$user.screen_name",
+                    retweet_count:  { "$ifNull": [ "$retweet_count" , 0] }
+
+
+                    }
+                }
+                ,
+                {
+                    "$sort": {
+                        "retweet_count": -1
+                    }
+                }
+
+                ,
+                {
+                    "$group": {"_id": "$screenName" ,
+                    "retweets": { $first: "$retweet_count" }
+                }
+                }
+           
+                ,
+                {
+                    "$sort": {
+                        "retweets": -1
+                    }
+                }
+
+        ]
+              ).then( data=>{
+
+                res.send( {status: true , data: data});
+            
+            
+           
+        } ).catch( err=>{
+            res.send( {status: false  , error: err.message});
+        } )
+    }
+});
+
 // Get   tweets between 
 router.post("/get/:username" , (req: any, res: any) => {
 
